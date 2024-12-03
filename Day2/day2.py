b50e9ed5547bb444fe6fbe21bafac7e92b06674b
roycarlton <carltonroy64@gmail.com>
2024-12-03 07:11:47 +0000

    Day 2 part 2 complete

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,18 +1,36 @@
-def is_safe(report):
+def apply_dampener(report):
+    for i in range(len(report)):
+        temp_report = report[:]
+        temp_report.pop(i)
+        if is_safe(temp_report, False):
+            return True
+    return False
+
+def is_safe(report, dampener):
     diff = report[1] - report[0]
     if abs(diff) > 3 or abs(diff) < 1:
-        return False
+        if dampener:
+            return apply_dampener(report)
+        else:
+            return False
     if diff > 0:
-        ascending = True
+        prev_ascending = True
     else:
-        ascending = False
+        prev_ascending = False
 
     for i in range(2, len(report)):
         diff = report[i] - report[i-1]
         if abs(diff) > 3 or abs(diff) < 1:
-            return False
-        if (diff > 0) != ascending:
-            return False
+            if dampener:
+                return apply_dampener(report)
+            else:
+                return False
+        if (diff > 0) != prev_ascending:
+            if dampener:
+                return apply_dampener(report)
+            else:
+                return False
+        prev_ascending = (diff > 0)
         
     return True
 
@@ -29,9 +47,21 @@ for report in data.split("\n"):
     for level in report.split(" "):
         formatted_report.append(int(level))
     
-    if is_safe(formatted_report):
+    if is_safe(formatted_report, False):
         total_safe += 1
 
 print(total_safe)
 
 #PART 2
+
+total_safe = 0
+
+for report in data.split("\n"):
+    formatted_report = []
+    for level in report.split(" "):
+        formatted_report.append(int(level))
+    
+    if is_safe(formatted_report, True):
+        total_safe += 1
+
+print(total_safe)
