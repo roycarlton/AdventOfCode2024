9c3282f7d37dd5e593fce225e8aaa39e2837f522
roycarlton <carltonroy64@gmail.com>
2024-12-03 00:40:27 +0000

    Part 2 of day 1 complete

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,3 +1,5 @@
+#Part 1
+
 file = open("input.txt", "r")
 numbers = file.read()
 file.close()
@@ -19,5 +21,20 @@ total = 0
 
 for i in range(len(list1)):
     total += abs(int(list1[i])-int(list2[i]))
+
+print(total)
+
+#PART 2
+
+total = 0
+
+for num in list1:
+    if num in list2:
+        count = 1
+        i = list2.index(num) + 1
+        while list2[i] == num:
+            count += 1
+            i += 1
+        total += (int(num) * count)
 
 print(total)
