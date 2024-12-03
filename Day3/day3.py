8c3c0d56dc3fd17a13c83a811918d455f4249860
roycarlton <carltonroy64@gmail.com>
2024-12-03 14:30:55 +0000

    day 3 part 2 complete

--- Day3/day3.py
+++ Day3/day3.py
@@ -13,3 +13,22 @@ for match in matches:
     total += (int(nums[0]) * int(nums[1]))
 
 print(total)
+
+#part 2
+
+matches = re.findall("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data)
+
+do = True
+total = 0
+
+for match in matches:
+    if match == "do()":
+        do = True
+    elif match == "don't()":
+        do = False
+    else:
+        if do:
+            nums = match[4:][:-1].split(",")
+            total += (int(nums[0]) * int(nums[1]))
+
+print(total)
